98ca14e97ea7a4e260e0d03a9393dd25773a2ab2
Danny Grove <groved@google.com>
2016-05-09 21:59:17 -0700

    Remove vendor scripts

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -25,23 +25,8 @@ gulp.task('compile_js', function() {
     fileName: 'build.min.js'
   };
 
-  var vendor_externs = plugins.filenames.get('vendor_externs');
-
-  if (!Array.isArray(vendor_externs)) {
-    vendor_externs = [vendor_externs];
-  }
-
-  vendor_externs = vendor_externs.map(function(path) {
-    var bower = config.Path.BOWER_FOLDER;
-    if (!/\/$/.test(bower)) {
-        bower += '/';
-    }
-    return bower + path; 
-  });
-
   var externs = [];
   externs.concat(plugins.filenames('closure_externs'));
-  externs.concat(vendor_externs);
 
   closureOpts.compilerFlags.externs = externs
 
@@ -52,7 +37,6 @@ gulp.task('compile_js', function() {
 
 gulp.task('minify_js', function() {
   return gulp.src([
-    config.Path.JS_TEMP_DIR + 'vendor.min.js',
     config.Path.JS_TEMP_DIR + 'build.min.js'
   ])
     .pipe(plugins.concat('main.min.js'))
@@ -62,25 +46,13 @@ gulp.task('minify_js', function() {
 
 gulp.task('build_js', function(callback) {
   return runSequence(
-      'build_deps',
-      'compile_js',
-      'minify_js',
-      callback);
+    'closure_externs',
+    'compile_js',
+    'minify_js',
+    callback);
 });
 
 gulp.task('closure_externs', function() {
     return gulp.src(config.Path.CLOSURE_EXTERNS)
         .pipe(plugins.filenames('closure_externs'))
 })
-
-gulp.task('build_deps', ['closure_externs'], function() {
-  var filterJs = plugins.filter(['**/*.js', '!**/*.min.js'], { restore: true });
-  return gulp.src('./bower.json')
-    .pipe(plugins.bower({base: './bower_components/'}))
-    .pipe(filterJs)
-    .pipe(plugins.filenames('vendor_externs'))
-    .pipe(plugins.concat('vendor.min.js'))
-    .pipe(plugins.uglify())
-    .pipe(filterJs.restore)
-    .pipe(gulp.dest(config.Path.JS_TEMP_DIR));
-});
